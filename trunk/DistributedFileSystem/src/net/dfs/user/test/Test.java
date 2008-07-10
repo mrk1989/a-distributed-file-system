6454b76af55bd433898b0cae51459632a730c883
rukshanrc <rukshanrc@f92553e1-dd50-0410-822f-3b4ca1d91bfb>
2008-07-10 07:25:35 +0000

    client class separated... with no exceptions.. :)

--- trunk/DistributedFileSystem/src/net/dfs/user/test/Test.java
+++ trunk/DistributedFileSystem/src/net/dfs/user/test/Test.java
@@ -3,15 +3,30 @@ package net.dfs.user.test;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+import org.springframework.context.ApplicationContext;
+
+import net.dfs.server.filesplitter.FileSplitService;
 import net.dfs.user.connect.StorageConnectionHandler;
 import net.dfs.user.connect.impl.StorageConnectionHandlerImplementation;
 
 public class Test {
 
-	public static void main(String args[]) throws FileNotFoundException{
-		String fileName = "C:\\Done.txt" ;
-		StorageConnectionHandler store = new StorageConnectionHandlerImplementation();
+	private static StorageConnectionHandler store;
 
-		store.StoreFile(new FileInputStream(fileName));
+	public static void main(String args[]) throws FileNotFoundException{
+		
+		FileInputStream fileIn = new FileInputStream("C:\\Done.txt");
+		System.out.println("Created...");
+		store.StoreFile(fileIn);
 	}
+
+	public static void setStore(StorageConnectionHandler store) {
+		Test.store = store;
+	}
+
+	public static StorageConnectionHandler getStore() {
+		return store;
+	}
+
+
 }
